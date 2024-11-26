70b0be317ec98092c7e6442e8fa65e00ab22ee7e
coderedge <coderedge@gmail.com>
2024-11-26 22:33:31 +0100

    Navbar with icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDom from "react-dom";
 
 import Navbar from "./Navbar/navbar"; //importation du composant navbar
-import './main.css'; //importation du fichier style main.css
-
+import "./main.css"; //importation du fichier style main.css
 
 export default function Main() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
     </div>
-  )
+  );
 }
 
 //Afficher le composant Main
-ReactDom.createRoot(document.querySelector('#root')).render(<Main/>)
+ReactDom.createRoot(document.querySelector("#root")).render(<Main />);
