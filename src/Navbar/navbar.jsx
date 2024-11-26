5d85d4c056c36d74352aed9c4f39a69b1896c1db
coderedge <coderedge@gmail.com>
2024-11-26 22:13:38 +0100

    Navbar with icons

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -19,22 +19,21 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar-header">
-                {/* <img src="logo_fablab.png" alt="Logo Fablab" className="logo" /> */}
-                <h1>Fablab ULC Icam</h1>
+                <img src="./src/assets/LogoFabLab.jpg" alt="Logo Fablab ULC" className="logo"/>
             </div>
             <ul className="nav-links">
-                {['Tableau de bord', 'Inventaire', 'Budget', 'Calendrier', 'Membres', 'Boutique', 'Parametre'].map((link, index) => (
+                {menuItems.map((link, index) => (
                     <li key={index}>
-                        {/* <img src={`logo${index + 1}.png`} alt={`Logo ${index + 1}`} className="nav-logo" /> */}
-                        <a href="#">{link}</a>
+                        <link.icon className="nav-logo"/>
+                        <a href={link.text}>{link.text}</a>
                     </li>
                 ))}
             </ul>
-
+            
             <hr />
 
             <div className="logout">
-                <FiLogOut className="logout-logo"/>   
+                <FiLogOut className="logout-logo"/> 
                 <a href="#" className="logout-link">Déconnexion</a>
             </div>
         </div>
