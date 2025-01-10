42684cc94d279fcc57aa3c22727681c11a3d4ab1
Remile Bianga <remilebianga44@gmail.com>
2025-01-10 15:14:04 +0100

    intentaire student v3

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -2,7 +2,7 @@ import './table.css'
 
 const DynamicTable = ({theadChild, tbodyChild}) => {
     return (
-        <div className="table">
+        <div className="table" id='table'>
             <table>
                 <thead>
                     {theadChild}
